de6bcb81cb023511f10ab547fc1e4b32fe8833c4
Emory Griffith <emorygriffith91@gmail.com>
2014-10-28 15:33:26 -0400

    styled clipboard, run basic tests

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('The Iron Yard Rocks');
+//Item Constructor
+
+var Item = function(options) {
+  options = options || {};
+  this.status = options.status || 'incomplete';
+  this.message = options.message || 'Type something here';
+};
+
+
+//Instances
+var item = new Item({
+  message: "Hey here's a test message!"
+});
+
+
+//Pass it into the document
+
+$('.list').append('<li>' + item.message + '<img src="http://png-4.findicons.com/files/icons/1580/devine_icons_part_2/128/trash_recyclebin_empty_closed.png" height=30px width=30px /></li>');
+
+
+$('.addbutton').on('click', function(){
+  $('.list').append('<li>' + item.message + '</li>');
+
+})
--- test/spec/test.js
+++ test/spec/test.js
@@ -3,11 +3,22 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-
+  describe('An Item', function () {
+    describe('Creating an item', function () {
+      it('should be an instance of Item', function () {
+        var item = new Item();
+        expect(item).to.be.an.instanceOf(Item);
+      });
+      it('should be incomplete by default', function() {
+        var item = new Item();
+        expect(item.status).to.equal('incomplete');
+      });
+      it('should populate with a default message', function() {
+        var item = new Item();
+        expect(item.message).to.equal('Type something here');
       });
     });
+
+
   });
 })();
