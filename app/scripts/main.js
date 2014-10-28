6845dc06a34602692432b3501232972011af8f69
Emory Griffith <emorygriffith91@gmail.com>
2014-10-28 15:50:14 -0400

    added form and linked it to li

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,22 +3,21 @@
 var Item = function(options) {
   options = options || {};
   this.status = options.status || 'incomplete';
-  this.message = options.message || 'Type something here';
 };
 
 
 //Instances
 var item = new Item({
-  message: "Hey here's a test message!"
 });
 
 
 //Pass it into the document
 
-$('.list').append('<li>' + item.message + '<img src="http://png-4.findicons.com/files/icons/1580/devine_icons_part_2/128/trash_recyclebin_empty_closed.png" height=30px width=30px /></li>');
 
 
 $('.addbutton').on('click', function(){
-  $('.list').append('<li>' + item.message + '</li>');
+  var input = $('#myform').val()
+
+  $('.list').append('<li>' + input + '</li>');
 
 })
